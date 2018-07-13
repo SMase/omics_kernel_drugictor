35d795cc144b149ddbb7fae65910c68a7a0712cb
SMase <mase@lifematics.co.jp>
2018-07-13 18:45:39 +0900

    add a part of bmtmkl algo

--- py_bmtmkl/BMTMKL.py
+++ py_bmtmkl/BMTMKL.py
@@ -1,12 +1,17 @@
 import pandas as pd
 import numpy as np
-
+from collections import OrderedDict
 
 class BayesianMultitaskMultipleKernelLearning:
 	def __init__(self, exp_path, tr_dr_path, ts_dr_path):
 		self._exp_df = pd.read_table(exp_path, index_col=0)
 		self._tr_dr_df = pd.read_table(tr_dr_path, index_col=0)
 		self._ts_dr_df = pd.read_table(ts_dr_path, index_col=0)
+		self._drug_list = [drug_name for drug_name in self._tr_dr_df.columns if drug_name in self._ts_dr_df.columns]
+
+	@property
+	def drug_list(self):
+		return self._drug_list
 
 	def get_matlix_rdf_kernel_dot(self, df1, df2, gamma):
 		diff = np.zeros((len(df1.columns), len(df2.columns)))
@@ -19,24 +24,30 @@ class BayesianMultitaskMultipleKernelLearning:
 		kernel_df.columns = df2.columns
 		return kernel_df
 
-	def get_kernel_dict(self):
-		tr_dict = {}
-		ts_dict = {}
-		drug_list = [drug_name for drug_name in self._tr_dr_df.columns if drug_name in self._ts_dr_df.columns]
-		print(drug_list)
-		for drug_name in drug_list:
-			tr_dr_cellline_index = self._tr_dr_df[drug_name].dropna(how='all').index
-			ts_dr_cellline_index = self._ts_dr_df[drug_name].dropna(how='all').index
+	def get_kernel_df(self, drug_name):
+		tr_dr_cellline_index = self._tr_dr_df[drug_name].dropna(how='all').index
+		ts_dr_cellline_index = self._ts_dr_df[drug_name].dropna(how='all').index
 
-			tr_cell_line_name = [name for name in tr_dr_cellline_index if name in list(self._exp_df.columns)]
-			ts_cell_line_name = [name for name in ts_dr_cellline_index if name in list(self._exp_df.columns)]
+		tr_cell_line_name = [name for name in tr_dr_cellline_index if name in list(self._exp_df.columns)]
+		ts_cell_line_name = [name for name in ts_dr_cellline_index if name in list(self._exp_df.columns)]
 
-			omics_tr_df = self._exp_df[tr_cell_line_name]
-			omics_ts_df = self._exp_df[ts_cell_line_name]
+		omics_tr_df = self._exp_df[tr_cell_line_name]
+		omics_ts_df = self._exp_df[ts_cell_line_name]
 
-			tr_dict[drug_name] = self.get_matlix_rdf_kernel_dot(omics_tr_df, omics_tr_df, 0.001)
-			ts_dict[drug_name] = self.get_matlix_rdf_kernel_dot(omics_ts_df, omics_tr_df, 0.001)
+		tr_kernel_df = self.get_matlix_rdf_kernel_dot(omics_tr_df, omics_tr_df, 0.001)
+		ts_kernel_df = self.get_matlix_rdf_kernel_dot(omics_ts_df, omics_tr_df, 0.001)
 
-		return tr_dict, ts_dict
+		return tr_kernel_df, ts_kernel_df
+
+
+
+
+
+
+
+
+
+
+
 
 
--- test/py_bmtmkl/test_bmtmkl.py
+++ test/py_bmtmkl/test_bmtmkl.py
@@ -2,7 +2,11 @@ import sys, os
 import unittest
 import pandas as pd
 import numpy as np
+import numpy.matlib
+
+
 from py_bmtmkl.BMTMKL import BayesianMultitaskMultipleKernelLearning
+
 
 
 class TestBayesianMultitaskMultipleKernelLearning(unittest.TestCase):
@@ -18,14 +22,196 @@ class TestBayesianMultitaskMultipleKernelLearning(unittest.TestCase):
 
 	def test_rbf_kernel_should_be_almost_equal_for_output_of_r_script(self):
 		# significant figure 18 is OK, but 19 is NG
-		tr_df_dict, _ = self.bmtmkl.get_kernel_dict()
+		tr_df, _ = self.bmtmkl.get_kernel_df('Drug1')
 		ans_df = pd.read_table('./test_data/r_rbf_train.tsv', index_col=0)
-		tr_df_dict['Drug1'] = tr_df_dict['Drug1'].ix[ans_df.columns, ans_df.columns]
+		tr_df = tr_df.ix[ans_df.columns, ans_df.columns]
 
 		for ii in range(len(ans_df.columns)):
 			np.testing.assert_almost_equal(
-				list(tr_df_dict['Drug1'].values[ii]),
+				list(tr_df.values[ii]),
 				list(ans_df.values[ii]), decimal=18,
 				err_msg='error index: ' + str(ii))
+
+	def test_train_bmtmkl_should_be_almost_equal_for_output_of_r_script(self):
+		tr_df_list = []
+		np.random.seed(1606)
+		r_a_mu_train = list(pd.read_table('./test_data/r_a_mu_train.tsv', header=None, index_col=0)[1])
+		r_G_mu_train = list(pd.read_table('./test_data/r_G_mu_train.tsv', header=None, index_col=0).T[1])
+
+		for drug_name in self.bmtmkl.drug_list:
+			print(drug_name)
+			tr_df, _ = self.bmtmkl.get_kernel_df(drug_name)
+			if drug_name == 'Drug1':
+				ans_df = pd.read_table('./test_data/r_rbf_train.tsv', index_col=0)
+				tr_df = tr_df.ix[ans_df.columns, ans_df.columns]
+				tr_df_list.append(tr_df)
+
+		log2pi = np.log(2 * np.pi)
+
+		T_len = len(tr_df_list)
+		D_mtx = np.zeros([T_len, 1], dtype=int)
+		N_mtx = np.zeros([T_len, 1], dtype=int)
+		lambda_alpha_list = []
+		lambda_beta_list = []
+		P = 1
+
+		for ii in range(T_len):
+			D_mtx[ii, 0] = tr_df_list[ii].shape[0]
+			N_mtx[ii, 0] = tr_df_list[ii].shape[1]
+
+		prm_alpha_lamda = 1
+		prm_beta_lamda = 1
+
+		for ii in range(T_len):
+			alpha_lamda_mtx = np.full([D_mtx[ii, 0], 1], prm_alpha_lamda + 0.5)
+			beta_lamda_mtx = np.full([D_mtx[ii, 0], 1], prm_beta_lamda)
+			lambda_alpha_list.append(alpha_lamda_mtx)
+			lambda_beta_list.append(beta_lamda_mtx)
+
+		prm_alpha_upsilon = 1
+		prm_beta_upsilon = 1
+
+		alpha_upsilon_mtx = np.full([T_len, 1], prm_alpha_upsilon + 0.5 * N_mtx * P)
+		beta_upsilon_mtx = np.full([T_len, 1], prm_beta_upsilon)
+
+		a_mu_list = []
+		a_sigma_list = []
+		G_mu_list = []
+		G_sigma_list = []
+		for ii in range(T_len):
+			a_mu_mtx = np.zeros([D_mtx[ii, 0], 1])
+			a_sigma_mtx = np.identity(D_mtx[ii, 0], int)
+
+			for iii in range(D_mtx[ii, 0]):
+				a_mu_mtx[iii, 0] = r_a_mu_train[iii]
+
+			a_mu_list.append(a_mu_mtx)
+			a_sigma_list.append(a_sigma_mtx)
+
+			G_mu_mtx = np.zeros([P, N_mtx[ii, 0]])
+			G_sigma_mtx = np.identity(P, int)
+
+			for iii in range(N_mtx[ii, 0]):
+				G_mu_mtx[P-1, iii] = r_G_mu_train[iii]
+
+			G_mu_list.append(G_mu_mtx)
+			G_sigma_list.append(G_sigma_mtx)
+
+		prm_alpha_gamma = 1
+		prm_beta_gamma = 1
+		prm_alpha_omega = 1
+		prm_beta_omega = 1
+		prm_alpha_epsilon = 1
+		prm_beta_epsilon = 1
+
+		alpha_gamma_mtx = np.full([T_len, 1], prm_alpha_gamma + 0.5)
+		beta_gamma_mtx = np.full([T_len, 1], prm_beta_gamma)
+		alpha_omega_mtx = np.full([P, 1], prm_alpha_omega + 0.5)
+		beta_omega_mtx = np.full([P, 1], prm_beta_omega)
+		alpha_epsilon_mtx = np.full([T_len, 1], prm_alpha_epsilon + 0.5 * N_mtx)
+		beta_epsilon_mtx = np.full([T_len, 1], prm_beta_epsilon)
+		be_mu_mtx = np.concatenate([np.zeros([T_len, 1]), np.ones([P, 1])])
+		be_sigma_mtx = np.identity(T_len + P, int)
+
+		kmkm_list = []
+		for ii in range(T_len):
+			kmkm_list.append(np.zeros([D_mtx[ii, 0], D_mtx[ii, 0]]))
+			for iii in range(P):
+				kmkm_list[ii] = kmkm_list[ii] + np.dot(tr_df_list[ii].values, tr_df_list[ii].T.values)
+
+			tr_df_list[ii] = np.full([D_mtx[ii, 0], N_mtx[ii, 0]*P], tr_df_list[ii])
+
+		prm_progress = 0
+		if prm_progress:
+			pass
+
+		atimesaT_mu_list = []
+		GtimesGT_mu_list = []
+		for ii in range(T_len):
+			atimesaT_mu_list.append(np.dot(a_mu_list[ii], a_mu_list[ii].T) + a_sigma_list[ii])
+			GtimesGT_mu_list.append(np.dot(G_mu_list[ii], G_mu_list[ii].T) + N_mtx[ii] * G_sigma_list[ii])
+
+		btimesbT_mu_mtx = np.dot(be_mu_mtx[0:T_len, 0], be_mu_mtx[0:T_len, 0].T) + be_sigma_mtx[0:T_len, 0:T_len]
+		etimeseT_mu_mtx = np.dot(be_mu_mtx[T_len:T_len+P, 0], be_mu_mtx[T_len:T_len+P, 0].T) + be_sigma_mtx[T_len:T_len+P, T_len:T_len+P]
+		etimesb_mu_mtx = np.zeros([P, T_len])
+		for ii in range(T_len):
+			etimesb_mu_mtx[:, ii] = be_mu_mtx[T_len:T_len+P, 0] * be_mu_mtx[ii, 0] + be_sigma_mtx[T_len:T_len+P, ii]
+
+		KmtimesGT_mu_list = []
+		for ii in range(T_len):
+			KmtimesGT_mu_list.append(np.dot(tr_df_list[ii], numpy.matlib.repmat(G_mu_list[ii].T, P, 1)))
+
+		prm_iter = 200
+
+		for itr in range(prm_iter):
+
+			for ii in range(T_len):
+				lambda_beta_list[ii] = 1 / (prm_beta_lamda + 0.5 * np.diag(atimesaT_mu_list[ii]))
+
+			aaa = 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
